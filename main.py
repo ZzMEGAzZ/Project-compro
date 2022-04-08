67f5a76aec288b3a3fba489be2fbfc70f70fb9a7
Apisit Sangkrajang <85662757+ZzMEGAzZ@users.noreply.github.com>
2022-04-08 15:38:17 +0000

    register

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 '''Group No.
-Project title: 
+Project title: Covid-19
 Subject: 1204111
 Semester: 02-2564
 Section: 6/16
-Project Members: 
+Project Members:
 1.Mr.Kristikorn ID:6410505442
 2.Mr.Saharat ID:6410505914
 3.Mr.Suwichan ID:6410505442
 4.Ms.Yosita ID:6410505817
 '''
+
+def register(): 
